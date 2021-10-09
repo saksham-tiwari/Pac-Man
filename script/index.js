33ab2ed40a97878dc1064a39adfef9887f2d67da
AlStien <aj601604@gmail.com>
2021-10-10 01:47:38 +0530

    initial level design

--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 let pacman = document.querySelector('#pacman');
 let pacman_Animation = ['url(img/Pacman-1.png)', 'url(img/Pacman-2.png)', 'url(img/Pacman-3.png)']
-let leftPos = 506;
+let leftPos = 430;
 var topPos = 370;
 let i=1;
 var speed = 6;
@@ -9,7 +9,7 @@ let b=false;
 let horizontalBoundary = document.querySelectorAll('.horizontal-line');
 let verticalBoundary = document.querySelectorAll('.vertical-line');
 
-console.log(horizontalBoundary[1].offsetTop);
+console.log(horizontalBoundary[2].offsetTop);
 
 setInterval(PacManAnimation, 115);
 function PacManAnimation()
@@ -22,38 +22,90 @@ function PacManAnimation()
 
 window.addEventListener("keypress", function(event){
     console.log(topPos);
-    if(event.key === "d")
+    if(event.key == "d")
     {
-        if(leftPos > parseInt(verticalBoundary[1].offsetLeft)-40)
+        pacman.style.transform = "rotate(0deg)";
+        if(topPos > parseInt(verticalBoundary[2].offsetTop) && topPos < parseInt(verticalBoundary[2].offsetTop + 310))
         {
-            pacman.style.transform = "rotate(0deg)";
+            if(leftPos < parseInt(verticalBoundary[2].offsetLeft)-60)
+            {
+                leftPos = leftPos + speed;
+                pacman.style.left = leftPos + "px";
+            }
+            if(leftPos < parseInt(verticalBoundary[3].offsetLeft)-60)
+            {
+                leftPos = leftPos + speed;
+                pacman.style.left = leftPos + "px";
+            }
+        }
+        else if(leftPos < parseInt(verticalBoundary[1].offsetLeft)-30)
+        {
             leftPos = leftPos + speed;
             pacman.style.left = leftPos + "px";
         }
     }
-    if(event.key === "a")
+    if(event.key == "a")
     {
-        if(leftPos < parseInt(verticalBoundary[0].offsetLeft)+40)
+        pacman.style.transform = "rotate(180deg)";
+        if(topPos > parseInt(verticalBoundary[2].offsetTop) && topPos < parseInt(verticalBoundary[2].offsetTop + 310))
         {
-            pacman.style.transform = "rotate(180deg)";
+            if(leftPos > parseInt(verticalBoundary[2].offsetLeft)+30)
+            {
+                leftPos = leftPos - speed;
+                pacman.style.left = leftPos + "px";
+            }
+            if(leftPos > parseInt(verticalBoundary[3].offsetLeft)-60)
+            {
+                leftPos = leftPos + speed;
+                pacman.style.left = leftPos + "px";
+            }
+}
+        else if(leftPos > parseInt(verticalBoundary[0].offsetLeft)+40)
+        {
             leftPos = leftPos - speed;
             pacman.style.left = leftPos + "px";
         }
     }
-    if(event.key === "w")
+    if(event.key == "w")
     {
-        if(topPos > parseInt(horizontalBoundary[0].offsetTop)+30)
+        pacman.style.transform = "rotate(-90deg)";
+        if(leftPos > parseInt(horizontalBoundary[2].offsetLeft) && leftPos < parseInt(horizontalBoundary[2].offsetLeft+910))
         {
-            pacman.style.transform = "rotate(-90deg)";
+            if(topPos > parseInt(horizontalBoundary[2].offsetTop)+30)
+            {
+                topPos = topPos - speed;
+                pacman.style.top = topPos + "px";
+            }
+            if(topPos > parseInt(horizontalBoundary[3].offsetTop))
+            {
+                topPos = topPos + speed;
+                pacman.style.top = topPos + "px";
+            }
+}
+        else if(topPos > parseInt(horizontalBoundary[0].offsetTop)+30)
+        {
             topPos = topPos - speed;
             pacman.style.top = topPos + "px";
         }
     }
-    if(event.key === "s")
+    if(event.key == "s")
     {
-        if(topPos < parseInt(horizontalBoundary[1].offsetTop)-60)
+        pacman.style.transform = "rotate(90deg)";
+        if(leftPos > parseInt(horizontalBoundary[3].offsetLeft-10) && leftPos < parseInt(horizontalBoundary[3].offsetLeft+910))
         {
-            pacman.style.transform = "rotate(90deg)";
+            if(topPos < parseInt(horizontalBoundary[3].offsetTop)-60)
+            {
+                topPos = topPos + speed;
+                pacman.style.top = topPos + "px";
+            }
+            if(topPos < parseInt(horizontalBoundary[2].offsetTop))
+            {
+                topPos = topPos - speed;
+                pacman.style.top = topPos + "px";
+            }
+        }
+        else if(topPos < parseInt(horizontalBoundary[1].offsetTop)-60)
+        {
             topPos = topPos + speed;
             pacman.style.top = topPos + "px";
         }
