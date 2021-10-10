937563f323277c91a92e8fa6c1b67200846d82ca
AlStien <aj601604@gmail.com>
2021-10-10 22:32:28 +0530

    updated layout

--- script/movement.js
+++ script/movement.js
@@ -1,30 +1,31 @@
-let leftPos = 605;
-var topPos = 520;
-var speed = 6;
+let leftPos = pacman.offsetLeft;
+var topPos = pacman.offsetTop;
+var speed = 5;
 
 let horizontalBoundary = document.querySelectorAll('.horizontal-line');
 let verticalBoundary = document.querySelectorAll('.vertical-line');
 
-console.log(horizontalBoundary[5].offsetTop);
+console.log(verticalBoundary[2].offsetLeft);
 
 window.addEventListener("keypress", function(event){
-    console.log(topPos);
-    // console.log(leftPos);
+    // console.log(topPos);
+    console.log(leftPos);
     if(event.key == "d")
     {
         pacman.style.transform = "rotate(0deg)";
-        if(topPos > parseInt(verticalBoundary[2].offsetTop) && topPos < parseInt(verticalBoundary[2].offsetTop + 310))
+        if(topPos > parseInt(verticalBoundary[2].offsetTop) && topPos < parseInt(verticalBoundary[2].offsetTop + 300))
         {
-            if(leftPos < parseInt(verticalBoundary[2].offsetLeft)-60)
+            if(leftPos > parseInt(verticalBoundary[2].offsetLeft) && leftPos < parseInt(verticalBoundary[3].offsetLeft)-60)
             {
+                console.log("b");
                 leftPos = leftPos + speed;
                 pacman.style.left = leftPos + "px";
             }
-            if(leftPos < parseInt(verticalBoundary[3].offsetLeft)-60)
-            {
-                leftPos = leftPos + speed;
-                pacman.style.left = leftPos + "px";
-            }
+            // if(leftPos < parseInt(verticalBoundary[3].offsetLeft)-60)
+            // {
+            //     leftPos = leftPos + speed;
+            //     pacman.style.left = leftPos + "px";
+            // }
         }
         else if(leftPos < parseInt(verticalBoundary[1].offsetLeft)-30)
         {
@@ -107,18 +108,34 @@ window.addEventListener("keypress", function(event){
         {
             if(leftPos > parseInt(horizontalBoundary[5].offsetLeft) && leftPos < parseInt(horizontalBoundary[5].offsetLeft+400))
             {
-                if(topPos > parseInt(horizontalBoundary[5].offsetTop)-30)
+                if(topPos < parseInt(horizontalBoundary[5].offsetTop)-60)
                 {
+                    topPos = topPos + speed;
                     pacman.style.top = topPos + "px";
-                    console.log("Blah 1-1");
                 }
             }
-            if(topPos < parseInt(horizontalBoundary[3].offsetTop)-60)
+            else if(leftPos > parseInt(horizontalBoundary[6].offsetLeft)-60 && leftPos < parseInt(horizontalBoundary[6].offsetLeft+200))
+            {
+                if(topPos > parseInt(horizontalBoundary[6].offsetTop) && topPos < parseInt(horizontalBoundary[3].offsetTop)-60)
+                {
+                    topPos = topPos + speed;
+                    pacman.style.top = topPos + "px";
+                }
+            }
+            else if(leftPos > parseInt(horizontalBoundary[7].offsetLeft)-60 && leftPos < parseInt(horizontalBoundary[7].offsetLeft+200))
+            {
+                if(topPos > parseInt(horizontalBoundary[7].offsetTop) && topPos < parseInt(horizontalBoundary[3].offsetTop)-60)
+                {
+                    topPos = topPos + speed;
+                    pacman.style.top = topPos + "px";
+                }
+            }
+            else if(topPos < parseInt(horizontalBoundary[3].offsetTop)-60)
             {
                 topPos = topPos + speed;
                 pacman.style.top = topPos + "px";
             }
-            if(topPos < parseInt(horizontalBoundary[2].offsetTop))
+            else if(topPos < parseInt(horizontalBoundary[2].offsetTop))
             {
                 topPos = topPos - speed;
                 pacman.style.top = topPos + "px";
